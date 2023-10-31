77c70ae77ed372709959007a2ddf662fd2f36e8f
Quy Van <111257575+Cinnasmuh@users.noreply.github.com>
2023-10-30 21:57:58 -0400

    Create 19_removenthnodefromendoflist.py

---
+++ 19_removenthnodefromendoflist.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
+
+        for _ in range(n + 1):
+            first = first.next
+
+        while first is not None:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+
+        return dummy.next
